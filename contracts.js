34cde1b3ec6e742206ede3ebafb1219bd75759a3
cryptofam <37367587+cryptofam@users.noreply.github.com>
2018-04-04 14:53:29 +0200

    Update contracts.js

--- contracts.js
+++ contracts.js
@@ -1,3 +1,9 @@
+let FETCHED_ABI = [{'inputs': [{'type': 'uint256', 'name': '_value'}], 'constant': 'False', 'name': 'set', 'outputs': [], 'stateMutability': 'nonpayable', 'payable': 'False', 'type': 'function'}, {'inputs': [], 'constant': 'True', 'name': 'get', 'outputs': [{'type': 'uint256', 'name': ''}], 'stateMutability': 'view', 'payable': 'False', 'type': 'function'}];
+
+let DEPLOYED_ADDRESS = "0x12e5ce91a54d25f6e9d9de9cf1027c4c02c8980a";
+
+var CONTRACT = web3.eth.contract(FETCHED_ABI);
+
 // solidity code code
 var source = "" +
 "pragma solidity ^0.4.6;" +
